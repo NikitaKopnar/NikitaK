51b1051596350322b7a2205069c375b8452633b3
NikitaKopnar <46027314+NikitaKopnar@users.noreply.github.com>
2025-12-31 11:45:58 +0530

    Content changes

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -11,7 +11,7 @@ export const HeroSection = ()=>{
                 <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2 ">kopnar </span>
             </h1>
             <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
-               Front-end developer with 4+ years of experience building scalable, High performance, elegant web experiences with Angular, React and modern Javascript
+               Front-end developer with 4+ years of experience in building scalable, High performance, elegant web experiences with Angular, React and modern Javascript
             </p>
             <div className="pt-4 opacity-0 animate-fade-in-delay-4">
                 <a href="#Projects" className="cosmic-button">
